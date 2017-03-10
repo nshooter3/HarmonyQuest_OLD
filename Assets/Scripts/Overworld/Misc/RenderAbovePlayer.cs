2387b8a6e5842df1fb23aca17149f20c890a041e
nshooter3 <nick.shooter@gmail.com>
2017-03-10 02:37:59 -0500

    Added renderaboveplayer script

---
+++ Assets/Scripts/Overworld/Misc/RenderAbovePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderAbovePlayer : MonoBehaviour {
+
+    SpriteRenderer ren;
+
+	// Use this for initialization
+	void Start () {
+        ren = GetComponent<SpriteRenderer>();
+        if (ren != null)
+        {
+            ren.sortingLayerName = "AbovePlayer";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
