d6ceb14b2478f7bfb9c8a8f91b14510255245e20
nshooter3 <nick.shooter@gmail.com>
2017-03-11 20:11:49 -0500

    Adjusted battle scene, added dodging

--- Assets/Scripts/Battle/PlayerMovementBattle.cs
+++ Assets/Scripts/Battle/PlayerMovementBattle.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class PlayerMovementBattle : MonoBehaviour {
 
-    public BoxCollider2D bound;
-
     public float speed, actualSpeed;
 
     public Transform rBullet, lBullet;
@@ -13,83 +11,93 @@ public class PlayerMovementBattle : MonoBehaviour {
 
     private Transform playerPos;
 
-    private float xBound, yBound, playerRadius, bulletCooldownMax, bulletCooldownCur;
+    private float bulletCooldownMax, bulletCooldownCur;
+
+    Vector3 initScale;
+
+    SpriteRenderer ren;
+
+    ParticleSystem dodge;
+
+    //Vars for dashing duration and direction
+    float dashTimer = 0, invulnTimer = 0, maxDashTimer = 0.05f, maxInvulnTimer = 0.025f, dashCooldown = 0, maxDashCooldown = 0.1f;
+    Vector3 dashDir;
+
+    Rigidbody2D rb;
 
     // Use this for initialization
     void Start () {
+        initScale = transform.localScale;
         speed = 6f;//Stored value
         actualSpeed = speed;//Modified value that is actually used
-        xBound = bound.size.x / 2f;
-        yBound = bound.size.y / 2f;
-        playerRadius = this.transform.localScale.x / 13f;
         playerPos = gameObject.transform;
         bulletCooldownMax = 0.05f;
         bulletCooldownCur = 0;
+        rb = GetComponent<Rigidbody2D>();
+        ren = GetComponent<SpriteRenderer>();
+        dodge = GetComponentInChildren<ParticleSystem>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        InputManager.instance.UpdateInput();
         CheckForKeyInput();
 	}
 
     private void Move(Vector3 dir)
     {
         //Applies movement based on vector3 from CheckForMove
-        playerPos.position = playerPos.position + dir*actualSpeed*Time.deltaTime;
-        BoundPlayer();
+        rb.velocity = dir * actualSpeed*0.75f;
     }
 
     private void CheckForKeyInput()
     {
-
-        //Check for various directional keys/combinations for movement
-        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
+        if (dashTimer > 0)
         {
-            Move(new Vector3(2, 2, 0));
-        }
-        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow))
-        {
-            Move(new Vector3(2, -2, 0));
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
-        {
-            Move(new Vector3(-2, 2, 0));
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow))
-        {
-            Move(new Vector3(-2, -2, 0));
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Move(new Vector3(2, 0, 0));
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Move(new Vector3(-2, 0, 0));
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
-            Move(new Vector3(0, 2, 0));
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            Move(new Vector3(0, -2, 0));
-        }
-
-        //Used to set speed depending on whether or not the player is dashing
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
-            actualSpeed = speed * 1.5f;
+            /*
+            if (invulnTimer > 0)
+            {
+                invulnTimer -= Time.deltaTime;
+            }
+            else if (invulnTimer <= 0)
+            {
+                ren.color = new Color(1, 1, 1, 1) ;
+            }*/
+            Move(dashDir * 1.3f);
+            dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0)
+            {
+                dashCooldown = maxDashCooldown;
+                ren.color = new Color(1, 1, 1, 1);
+            }
         }
         else
         {
-            actualSpeed = speed;
-        }
+            if (dashCooldown > 0)
+            {
+                dashCooldown -= Time.deltaTime;
+                if (dashCooldown <= 0)
+                {
+                    dodge.Stop();
+                }
+            }
+            CheckDirectionalMovement();
 
+            //Used to set speed depending on whether or not the player is dashing
+            if (InputManager.instance.shiftPress && rb.velocity.magnitude > 0 && dashCooldown <= 0)
+            {
+                dashTimer = maxDashTimer;
+                invulnTimer = maxInvulnTimer;
+                dashDir = rb.velocity;
+                ren.color = new Color(1, 1, 1, 0.1f);
+                dodge.Play();
+                StartCoroutine(SizePulse());
+            }
+        }
         //Used to determine firing patterns
         if (bulletCooldownCur <= 0)
         {
-            if (Input.GetKey(KeyCode.A))
+            if (InputManager.instance.confirmHeld)
             {
                 Instantiate(bullets[0], rBullet.transform.position, Quaternion.identity);
                 Instantiate(bullets[0], lBullet.transform.position, Quaternion.identity);
@@ -102,22 +110,64 @@ public class PlayerMovementBattle : MonoBehaviour {
         }
     }
 
-    private void BoundPlayer()
+    private void CheckDirectionalMovement()
     {
-        //Prevents the player from exiting a specified x and y bound, keeps them in the arena
-        if (playerPos.position.x + playerRadius > xBound)
-            playerPos.position = new Vector3(xBound - playerRadius, playerPos.position.y, playerPos.position.z);
-        else if (playerPos.position.x - playerRadius < -xBound)
-            playerPos.position = new Vector3(-xBound + playerRadius, playerPos.position.y, playerPos.position.z);
-
-        if (playerPos.position.y + playerRadius > yBound)
-            playerPos.position = new Vector3(playerPos.position.x, yBound - playerRadius, playerPos.position.z);
-        else if (playerPos.position.y - playerRadius < -yBound)
-            playerPos.position = new Vector3(playerPos.position.x, -yBound + playerRadius, playerPos.position.z);
+        //Check for various directional keys/combinations for movement
+        if (InputManager.instance.rightHeld && InputManager.instance.upHeld)
+        {
+            Move(new Vector3(0.67f, 0.67f, 0));
+        }
+        else if (InputManager.instance.rightHeld && InputManager.instance.downHeld)
+        {
+            Move(new Vector3(0.67f, -0.67f, 0));
+        }
+        else if (InputManager.instance.leftHeld && InputManager.instance.upHeld)
+        {
+            Move(new Vector3(-0.67f, 0.67f, 0));
+        }
+        else if (InputManager.instance.leftHeld && InputManager.instance.downHeld)
+        {
+            Move(new Vector3(-0.67f, -0.67f, 0));
+        }
+        else if (InputManager.instance.rightHeld)
+        {
+            Move(new Vector3(1, 0, 0));
+        }
+        else if (InputManager.instance.leftHeld)
+        {
+            Move(new Vector3(-1, 0, 0));
+        }
+        else if (InputManager.instance.upHeld)
+        {
+            Move(new Vector3(0, 1, 0));
+        }
+        else if (InputManager.instance.downHeld)
+        {
+            Move(new Vector3(0, -1, 0));
+        }
+        else
+        {
+            Move(new Vector3(0, 0, 0));
+        }
     }
 
     public void setBulletCooldown(float val)
     {
         bulletCooldownMax = val;
     }
+
+    //Size effect to juice up dodges
+    IEnumerator SizePulse()
+    {
+        float rotDir = (Random.Range(0, 2) - 0.5f)*40;
+        for (float f = 1; f >= 0; f -= 0.1f)
+        {
+            transform.eulerAngles = Vector3.Lerp(Vector3.zero, new Vector3(0, 0, rotDir), f);
+            transform.localScale = initScale * (1f + f/2f);
+            yield return new WaitForSeconds(0.005f);
+        }
+        transform.eulerAngles = Vector3.zero;
+        yield return null;
+    }
+
 }
