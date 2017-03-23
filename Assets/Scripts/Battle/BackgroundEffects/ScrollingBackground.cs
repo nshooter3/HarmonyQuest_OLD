5db2a931569194529dc2f0f2648790e3226c2e80
nshooter3 <nick.shooter@gmail.com>
2017-03-23 17:34:12 -0400

    Scrolling battle background

---
+++ Assets/Scripts/Battle/BackgroundEffects/ScrollingBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    public float xSpeed = 0, ySpeed = 0;
+    Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mat.mainTextureOffset = new Vector2(mat.mainTextureOffset.x + xSpeed*Time.deltaTime, mat.mainTextureOffset.y + ySpeed * Time.deltaTime);
+    }
+}
