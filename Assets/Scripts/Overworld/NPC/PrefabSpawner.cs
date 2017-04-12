61271ab643c6652f3756a29d81cd0a40f8fd53e4
nshooter3 <nick.shooter@gmail.com>
2017-04-12 15:12:23 -0400

    Replaced emotespawner with generic prefab spawner

---
+++ Assets/Scripts/Overworld/NPC/PrefabSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour {
+
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        prefab = Instantiate(prefab, transform.parent);
+        prefab.transform.position = transform.position;
+        prefab.transform.eulerAngles = transform.eulerAngles;
+        prefab.transform.localScale = transform.localScale;
+        Destroy(gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
