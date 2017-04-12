4a8cc8fd73aa64e584bb8975e69c124cd73c0dd1
nshooter3 <nick.shooter@gmail.com>
2017-04-11 22:09:29 -0400

    Sprite layers can now adjust based on player pos

---
+++ Assets/Scripts/Overworld/Misc/AdjustLayerBasedOnPlayerPos.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustLayerBasedOnPlayerPos : MonoBehaviour {
+
+    //Adjusts layer based on player position, allows player to go in front of/behind sprites
+
+    string initLayer;
+    public string adjustedLayer = "AbovePlayer";
+    SpriteRenderer sr;
+    PlayerMovementOverworld player;
+    bool abovePlayer = true;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        initLayer = sr.sortingLayerName;
+        player = FindObjectOfType<PlayerMovementOverworld>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player != null)
+        {
+            if (player.transform.position.y < transform.position.y)
+            {
+                if (abovePlayer)
+                {
+                    sr.sortingLayerName = initLayer;
+                    abovePlayer = false;
+                }
+            }
+            else if (player.transform.position.y > transform.position.y) {
+                if (!abovePlayer)
+                {
+                    sr.sortingLayerName = adjustedLayer;
+                    abovePlayer = true;
+                }
+            }
+        }
+	}
+}
