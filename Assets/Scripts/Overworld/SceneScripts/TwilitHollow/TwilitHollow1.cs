0fb4478898537fb34b81d3023af797d99ebf6ee2
nshooter3 <nick.shooter@gmail.com>
2017-04-13 22:59:59 -0400

    Added dialogue to cutscene

--- Assets/Scripts/Overworld/SceneScripts/TwilitHollow/TwilitHollow1.cs
+++ Assets/Scripts/Overworld/SceneScripts/TwilitHollow/TwilitHollow1.cs
@@ -8,6 +8,7 @@ public class TwilitHollow1 : SceneScript {
     public CutsceneEvent dad, cam, player;
     public GameObject bike, bikePile, bikeDes, fishingBike, fishingBaby;
     Vector3 bikePileLower;
+    public GameObject dialogue;
     //Camera mounts
     public Transform cMount1, cMount2, cMount3, dMount1, dMount2, dMount3;
     public FishingRod rod;
@@ -21,7 +22,53 @@ public class TwilitHollow1 : SceneScript {
         fadedCol.w = 0;
         author.color = fadedCol;
         bikePileLower = new Vector3(bikePile.transform.position.x, bikePile.transform.position.y - 0.05f, bikePile.transform.position.z);
-        //InitFishingCutscene();
+        InitFishingCutscene();
+    }
+
+    void Update()
+    {
+        if (PlayerMovementOverworld.instance.mName == "CutsceneWait1")
+        {
+            CutsceneWait1();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
+        else if (PlayerMovementOverworld.instance.mName == "CutsceneReelIn1")
+        {
+            CutsceneReelIn1();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
+        else if (PlayerMovementOverworld.instance.mName == "CutsceneThrowBike")
+        {
+            CutsceneThrowBike();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
+        else if (PlayerMovementOverworld.instance.mName == "CutsceneLowerBait1")
+        {
+            CutsceneLowerBait1();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
+        else if (PlayerMovementOverworld.instance.mName == "CutsceneGrabBaby")
+        {
+            CutsceneGrabBaby();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
+        else if (PlayerMovementOverworld.instance.mName == "CutsceneLookAround")
+        {
+            CutsceneLookAround();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
+        else if (PlayerMovementOverworld.instance.mName == "CutsceneEnd")
+        {
+            CutsceneEnd();
+            PlayerMovementOverworld.instance.mName = "";
+            PlayerMovementOverworld.instance.mData = "";
+        }
     }
 
     void InitFishingCutscene()
@@ -36,8 +83,31 @@ public class TwilitHollow1 : SceneScript {
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(3.5f); }));
         cam.actionQueue.Enqueue(new Action(() => { GlobalFunctions.instance.AdjustColorOverTimeTextMesh(normCol, fadedCol, 1, author); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(2f); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.InitDialogue(PlayerMovementOverworld.instance, dialogue);})) ;
+    }
+
+    public void CutsceneWait1()
+    {
+        Debug.Log("yeah");
+        //CutsceneWait1
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(3f); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOnDialogueUI(); }));
+    }
+
+    public void CutsceneReelIn1()
+    {
+        // CutsceneReelIn1
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
         cam.actionQueue.Enqueue(new Action(() => { rod.Interact(); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(3.5f); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOnDialogueUI(); }));
+    }
+
+    public void CutsceneThrowBike()
+    {
+        //CutsceneThowBike
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Move(cMount3.position, 2f); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1); }));
         cam.actionQueue.Enqueue(new Action(() => { dad.ChangeDirectionDiagonal(false, false); }));
@@ -52,13 +122,28 @@ public class TwilitHollow1 : SceneScript {
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1.55f); }));
         cam.actionQueue.Enqueue(new Action(() => { dad.ChangeDirectionDiagonal(true, false); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1f); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOnDialogueUI(); }));
+    }
+
+    public void CutsceneLowerBait1(){
+        //CutsceneLowerBait1
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1f); }));
         cam.actionQueue.Enqueue(new Action(() => { rod.Interact(); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(5f); }));
         cam.actionQueue.Enqueue(new Action(() => { rod.Interact("baby"); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(3.5f); }));
         cam.actionQueue.Enqueue(new Action(() => { SFXManager.instance.Spawn("CryingBaby1"); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(4f); }));
-        cam.actionQueue.Enqueue(new Action(() => { dad.GetComponentInChildren<EmoteAnimator>().SetEmotion("Surprise") ; }));
+        cam.actionQueue.Enqueue(new Action(() => { dad.GetComponentInChildren<EmoteAnimator>().SetEmotion("Surprise"); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(0.5f); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOnDialogueUI(); }));
+    }
+
+    public void CutsceneGrabBaby()
+    {
+        //CutsceneGrabBaby
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(0.5f); }));
         cam.actionQueue.Enqueue(new Action(() => { fishingBaby.SetActive(false); }));
         cam.actionQueue.Enqueue(new Action(() => { dad.Move(dMount2.position, 4); }));
@@ -68,13 +153,36 @@ public class TwilitHollow1 : SceneScript {
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1f); }));
         cam.actionQueue.Enqueue(new Action(() => { SFXManager.instance.Spawn("CryingBaby2"); }));
         cam.actionQueue.Enqueue(new Action(() => { cam.Wait(5f); }));
-        cam.actionQueue.Enqueue(new Action(() => { dad.Move(dMount3.position, 4); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOnDialogueUI(); }));
+    }
 
+    public void CutsceneLookAround()
+    {
+        //CutsceneLookAround
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1); }));
+        cam.actionQueue.Enqueue(new Action(() => { dad.ChangeDirectionDiagonal(false, false); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(0.5f); }));
+        cam.actionQueue.Enqueue(new Action(() => { dad.ChangeDirectionDiagonal(false, true); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(0.5f); }));
+        cam.actionQueue.Enqueue(new Action(() => { dad.ChangeDirectionDiagonal(true, true); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(0.5f); }));
+        cam.actionQueue.Enqueue(new Action(() => { dad.ChangeDirectionDiagonal(true, false); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOnDialogueUI(); }));
+    }
+
+    public void CutsceneEnd()
+    {
+        //CutsceneEnd
+        cam.actionQueue.Enqueue(new Action(() => { cam.ToggleOffDialogueUI(); }));
+        cam.actionQueue.Enqueue(new Action(() => { cam.Wait(1); }));
+        cam.actionQueue.Enqueue(new Action(() => { dad.Move(dMount3.position, 4); }));
         cam.actionQueue.Enqueue(new Action(() => { PlayerMovementOverworld.instance.InitPlayerDefault(); }));
     }
-	
-	// Update is called once per frame
-	public override void UpdateScene () {
-		
-	}
+
+    // Update is called once per frame
+    public override void UpdateScene () {
+        
+    }
 }
