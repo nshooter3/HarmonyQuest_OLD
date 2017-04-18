f64356eaf7a7db5b855b6a16c997c5d1aa8bea92
nshooter3 <nick.shooter@gmail.com>
2017-04-17 23:45:53 -0400

    Redid scene2, added rock explosion function

--- Assets/Scripts/Overworld/SceneScripts/TwilitHollow/TwilitHollow2_Boulder.cs
+++ Assets/Scripts/Overworld/SceneScripts/TwilitHollow/TwilitHollow2_Boulder.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class TwilitHollow2_Boulder : SceneScript {
 
+    public GameObject dad, rock;
+    public Transform d1, d2, d3, d4, c1;
+    public ParticleSystem rockExplode, rockSmoke;
+    public CutsceneTrigger ct1;
+
+    bool rockExploded = false;
+
     // Use this for initialization
     public override void StartScene()
     {
@@ -12,11 +19,23 @@ public class TwilitHollow2_Boulder : SceneScript {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (ct1.isCol)
+        {
+
+        }
 	}
 
     public override void UpdateScene()
     {
 
+    }
+
+    IEnumerator BlowUpRock()
+    {
+        rockExplode.Play();
+        rockSmoke.Play();
+        rockExploded = true;
+        yield return new WaitForSeconds(.25f);
+        rock.SetActive(false);
     }
 }
