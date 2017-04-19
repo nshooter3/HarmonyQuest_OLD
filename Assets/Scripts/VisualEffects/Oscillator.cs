a2393aa0e374f683fbf3e6fa801f20280b5cecd9
nshooter3 <nick.shooter@gmail.com>
2017-04-18 22:51:18 -0400

    Added lake lillies and oscillator script

---
+++ Assets/Scripts/VisualEffects/Oscillator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Oscillator : MonoBehaviour {
+
+    Vector3 initPos;
+    public float xDis, yDis, speed;
+    public bool isOffset = false;
+    private float offset = 0;
+
+	// Use this for initialization
+	void Start () {
+        initPos = gameObject.transform.localPosition;
+        if (isOffset)
+        {
+            offset = Random.Range(1f, 10f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.localPosition = new Vector3(initPos.x + xDis * Mathf.Sin((Time.time + offset) * speed), initPos.y + yDis * Mathf.Sin((Time.time + offset) * speed), initPos.z);
+	}
+
+}
