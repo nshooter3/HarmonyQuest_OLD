97eec9844ed53dd019b5dab46d9ceeb90a4b8d64
nshooter3 <nick.shooter@gmail.com>
2017-04-18 22:22:06 -0400

    Added lake tiles

---
+++ Assets/Scripts/Overworld/SceneScripts/TwilitHollow/TwilitHollow4_Lake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TwilitHollow4_Lake : SceneScript {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Use this for initialization
+    public override void StartScene()
+    {
+
+    }
+
+    // Update is called once per frame
+    public override void UpdateScene()
+    {
+
+    }
+}
