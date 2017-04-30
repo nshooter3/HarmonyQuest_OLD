976c599a83ab2fb3ccd83b8f1177181b3f7e9239
nshooter3 <nick.shooter@gmail.com>
2017-04-30 16:39:07 -0400

    Added scene6

---
+++ Assets/Scripts/Overworld/SceneScripts/TwilitHollow/TwilitHollow6_Box2.cs
@@ -0,0 +1,20 @@
+﻿public class TwilitHollow6_Box2 : SceneScript
+{
+
+    // Use this for initialization
+    public override void StartScene()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateQueue();
+    }
+
+    public override void UpdateScene()
+    {
+
+    }
+}
