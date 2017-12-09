6b237726085694515a5efd891158ff974ca44784
nshooter3 <nick.shooter@gmail.com>
2017-12-09 13:26:12 -0500

    Added SetActiveAtStart script/prefab

---
+++ Assets/Scripts/General/SetActiveAtStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveAtStart : MonoBehaviour {
+
+    //Used to enable a group of objects upon awake. Hopefully this will help clean up my dev view
+    public GameObject[] objects;
+
+    void Awake()
+    {
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
